f7258de36979b3315d4d9cdf80d415f3efed472b
pmmccorkell <root@WRK1080681GOVT.academy.usna.edu>
2024-02-26 09:31:21 -0500

    WSL Commit

--- math.py
+++ math.py
@@ -2,16 +2,9 @@
 # Feb 2024
 
 from time import sleep
-import openai
-import logging
-import logging.handlers
-from datetime import datetime
-from gc import collect as trash
-import os
-import atexit
-from secrets import *
+from gc import cololect as trash
 from gpt import Chat
-
+import atexit
 
 def main():
 	session = Chat(name='Matlab',model='gpt-3.5-turbo-0125',instructions='You are an AI assistant that helps users generate Matlab code.')
