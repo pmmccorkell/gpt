00f3fbde50a09f31a804f7662b0958708fdd049f
pmmccorkell <root@WRK1080681GOVT.academy.usna.edu>
2024-02-26 10:41:13 -0500

    WSL Commit

--- matlab_example.py
+++ matlab_example.py
@@ -7,12 +7,18 @@ from gpt import Chat
 import atexit
 
 def main():
+	# Initiate class with default values.
 	session = Chat('Matlab','gpt-3.5-turbo-0125','You are an AI assistant that helps users generate Matlab code.')
+
+	# Register exit_program() to execute any time the program ends.
 	atexit.register(session.exit_program)
+
+	# Loop the prompt->answer cycle
 	while(session.loop):
 		session.new_message()
 		trash()
 		sleep(0.3)
 
+# Execute main() if this is the top level script.
 if __name__ == '__main__':
 	main()
